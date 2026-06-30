9579805fecc1dcb28a2d0b5826a5f2576d2c7f39
junior <junior.pihuave@gmail.com>
2026-06-30 13:37:18 -0500

    actualización

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,9 +1,7 @@
 import { Router } from "express";
-import { guardarPedido } from "../controladores/pedidos.ctrl.js"; // Asegúrate de que esta ruta sea correcta
+import { guardarPedido } from "../controladores/pedidos.ctrl.js";
 
 const router = Router();
-
-// Definir la ruta para crear un pedido
 router.post("/pedidos", guardarPedido);
 
 export default router;
